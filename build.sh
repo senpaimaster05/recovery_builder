353ef3df81ef1c332765929c1f8bb08dcd33e8a0
master <85785239+senpaimaster05@users.noreply.github.com>
2021-09-26 23:49:23 +0800

    DANDELION

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 
 MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp"
 DEVICE=dandelion
-DT_LINK="https://github.com/senpaimaster05/twrp_device_xiaomi_dandelion"
+DT_LINK="https://github.com/senpaimaster05/twrp_device_xiaomi_dandelion -b test"
 DT_PATH=device/xiaomi/$DEVICE
 
 echo " ===+++ Setting up Build Environment +++==="
